2a4ad45ea6664a8f9460c2939c36277b3d1958f3
Jared Beckler <jared.beckler@gmail.com>
2016-01-15 13:33:08 -0800

    add content to hawaii results

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,19 @@ $(document).ready(function() {
 
     if (userAnswer <= 9 && userAnswer >=5) {
       $("#resultsJumbo").show();
+      $("#resultsJumbo").addClass("hawaiiBG")
       $("#hawaii").show();
       $(".introQuest").hide();
     }
     else if (userAnswer >= 13) {
       $("#resultsJumbo").show();
+      $("#resultsJumbo").addClass("newYorkBG")
       $("#newYork").show();
       $(".introQuest").hide();
     }
     else if (userAnswer >= 10 && userAnswer <= 12){
       $("#resultsJumbo").show();
+      $("#resultsJumbo").addClass("yellowStoneBG")
       $("#yellowStone").show();
       $(".introQuest").hide();
     }
