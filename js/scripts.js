f54700ce664aac47c7f7fe831e19249fcb1cecd0
Jared Beckler <jared.beckler@gmail.com>
2016-01-15 12:57:04 -0800

    add jquery to hide/show content based on results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-  $("form#questions").submit(function(){
+  $("#blanks form").submit(function(event){
+    var answers = ["plan", "sleep", "food", "drinks", "distance"];
+    var userAnswer = 0;
+
+    answers.forEach(function(answer) {
+      userAnswer += parseInt($("select#" + answer).val());
+    });
+
+    if (userAnswer <= 9 && userAnswer >=5) {
+      $("#resultsJumbo").show();
+      $("#hawaii").show();
+      $(".introQuest").hide();
+    }
+    else if (userAnswer >= 13) {
+      $("#resultsJumbo").show();
+      $("#newYork").show();
+      $(".introQuest").hide();
+    }
+    else if (userAnswer >= 10 && userAnswer <= 12){
+      $("#resultsJumbo").show();
+      $("#yellowStone").show();
+      $(".introQuest").hide();
+    }
+    else {
+      alert("Please answer all the questions!");
+    }
 
 
     event.preventDefault();
